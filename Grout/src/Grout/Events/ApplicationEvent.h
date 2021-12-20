62b8b054a46edfb1d448a9b52a4346c510586d2f
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2021-12-20 00:19:57 -0300

    Not yet ready events but just a backup

--- Grout/src/Grout/Events/ApplicationEvent.h
+++ Grout/src/Grout/Events/ApplicationEvent.h
@@ -2,11 +2,34 @@
 
 #include "Event.h"
 
+#include <sstream>
+
 namespace Grout {
 	class GROUT_API WindowResizeEvent : public Event {
 	public:
+		// --- Constructor ---
 		WindowResizeEvent(int width, int height) : m_Width(width), m_Height(height) {}
 
+		// ----- Getters -----
+		inline int getWidth() const { return m_Width; }
+		inline int getHeight() const { return m_Height; }
+		// --------------------
 
+		// Debug default ToString()
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "WindowResizeEvent: " << m_Width << ", " << m_Height;
+			return ss.str();
+		}
+
+		// Those override parent's functions to return the
+		// correspondent type and category
+		EVENT_CLASS_TYPE(WindowResize)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+
+	private:
+		// Size in pixels of the window
+		int m_Width;
+		int m_Height;
 	};
 }
--- Grout/src/Grout/Events/KeyEvent.h
+++ Grout/src/Grout/Events/KeyEvent.h
@@ -1,1 +1,22 @@
 #pragma once
+ 
+#include "Event.h"
+
+#include <sstream>
+
+namespace Grout {
+	class GROUT_API KeyEvent : public Event {
+	public:
+		// Getter
+		inline int getKeyCode() const {
+			return m_KeyCode;
+		}
+
+		EVENT_CLASS_CATEGORY(EventCategoryApplication | EventCategoryInput)
+	protected:
+		// Protected constructor
+		KeyEvent(int keycode) : m_KeyCode(keycode) {}
+		// Holds the ID of the key belonging to this Event
+		int m_KeyCode;
+	};
+}
