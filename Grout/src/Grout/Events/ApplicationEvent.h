108e56f7bcac3900dc11a466509fd908906192cb
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2021-12-20 01:19:21 -0300

    Window and App Events

--- Grout/src/Grout/Events/ApplicationEvent.h
+++ Grout/src/Grout/Events/ApplicationEvent.h
@@ -8,7 +8,8 @@ namespace Grout {
 	class GROUT_API WindowResizeEvent : public Event {
 	public:
 		// --- Constructor ---
-		WindowResizeEvent(int width, int height) : m_Width(width), m_Height(height) {}
+		WindowResizeEvent(int width, int height) 
+		: m_Width(width), m_Height(height) {}
 
 		// ----- Getters -----
 		inline int getWidth() const { return m_Width; }
@@ -33,5 +34,56 @@ namespace Grout {
 		int m_Height;
 	};
 
-	
+	class GROUT_API WindowCloseEvent : public Event {
+	public:
+		WindowCloseEvent() {}
+
+		EVENT_CLASS_TYPE(WindowClose)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class GROUT_API WindowMovedEvent : public Event {
+	public:
+		// ---- Constructor ----
+		WindowMovedEvent(int windowX, int windowY)
+			: m_WindowX(windowX), m_WindowY(windowY) {}
+
+		// Debug default ToString()
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "WindowMovedEvent: " << m_WindowX << ", " << m_WindowY;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(WindowMoved)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	private:
+		// New position data
+		int m_WindowX;
+		int m_WindowY;
+	};
+
+	class GROUT_API AppTicketEvent : public Event {
+	public:
+		AppTicketEvent() {}
+
+		EVENT_CLASS_TYPE(AppTicket)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class GROUT_API AppUpdateEvent : public Event {
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class GROUT_API AppRenderEvent : public Event {
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+		EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
 }
