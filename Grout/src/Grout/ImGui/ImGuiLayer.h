706d0f2ff7e7223209b0d0b09b872fd1f047c7e5
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2021-12-25 02:47:27 -0300

    Creating the ImGui Layer

---
+++ Grout/src/Grout/ImGui/ImGuiLayer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Grout/Layer.h"
+
+namespace Grout {
+	class GROUT_API ImGuiLayer : public Layer
+	{
+	public:
+		ImGuiLayer();
+		~ImGuiLayer();
+
+		void OnAttach();
+		void OnDetach();
+
+		void OnUpdate();
+		void OnEvent(Event& event);
+
+	private:
+	};
+}
+
