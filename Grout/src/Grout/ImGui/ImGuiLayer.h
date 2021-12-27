397668a644bec8f9e8a1660b0b8ebd08c4fc5f73
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2021-12-27 10:04:16 -0300

    ImGui Events

--- Grout/src/Grout/ImGui/ImGuiLayer.h
+++ Grout/src/Grout/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Grout/Layer.h"
+#include "Grout/Events/MouseEvent.h"
+#include "Grout/Events/KeyEvent.h"
+#include "Grout/Events/ApplicationEvent.h"
 
 namespace Grout {
 	class GROUT_API ImGuiLayer : public Layer
@@ -14,6 +17,19 @@ namespace Grout {
 
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		// Callback functions
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
+		bool OnWindowClosedEvent(WindowCloseEvent& e);
 
 	private:
 		float time_ = 0.0f;
